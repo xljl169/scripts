646964af02ce3baec46a464772955b3e43ba963a
xljl169 <lihuizhong620@gmail.com>
2022-09-20 12:43:23 +0800

    Update ylgy.js

--- ylgy.js
+++ ylgy.js
@@ -3,7 +3,19 @@ var url = $request.url
 
 if (body) {
   var obj = JSON.parse($response.body)
-  obj.data ='046ef1bab26e5b9bfe2473ded237b572'
+  // obj.data ={
+  //   "map_md5" : [
+  //     "046ef1bab26e5b9bfe2473ded237b572",
+  //     "046ef1bab26e5b9bfe2473ded237b572"
+  //   ],
+  //   "map_seed" : [
+  //     1695523272,
+  //     455944082,
+  //     3477455185,
+  //     895092858
+  //   ]
+  // }
+  obj.data.map_md5 = ['046ef1bab26e5b9bfe2473ded237b572', '046ef1bab26e5b9bfe2473ded237b572']
   $done({ body: JSON.stringify(obj) })
 } else {
   $done({})
